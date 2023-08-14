24f2d3560f4acf3baf5592e9ef5be947c11c1366
Javlonbek Saydullayev <88250263+Saydullayev017@users.noreply.github.com>
2023-08-14 20:52:38 +0300

    lessons
    lesson1

---
+++ lesson1/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('hello TS');
+console.log('me');
+//# sourceMappingURL=index.js.map
---
+++ lesson1/index.ts
@@ -0,0 +1,33 @@
+// Asosiy malumotlar
+
+// let a = 1;
+let a: number = 1;
+
+// let b = 'text'
+let b: string = 'text';
+
+// let c = true;
+let c: boolean = true;
+
+// let d = null
+let d: null = null;
+
+// let e = undefined
+let e: undefined = undefined;
+
+// let f = {}
+// let f: object = {}; //TS
+let f: {name: string} = {name: 'Javlonbek'};
+f.name = 'Ismoil';
+
+
+let g: any;
+
+g = 12;
+g = 'text';
+g = false;
+g = {name: 'Mehmet'};
+g = function() {};
+g = [];
+g = null;
+g = undefined;
