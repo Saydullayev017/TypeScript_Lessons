a02f7e4ff7efcf457cc21c0b55b89504a56a2503
Javlonbek Saydullayev <88250263+Saydullayev017@users.noreply.github.com>
2023-08-21 21:49:37 +0300

    lesson5

--- lesson5/index.js
+++ lesson5/index.js
@@ -1,3 +1,23 @@
-"use strict";
-console.log('l');
-//# sourceMappingURL=index.js.map
+// TS da sinflar or classes
+// Agar class ni ichida function yozadigon bolsek bu method deb atalaid,
+// Agar class dan tashqarida function yozadigon bolsak bu funtion deb ataladi.
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this._name = 'Javlon';
+        this._age = 21;
+        this._name = name;
+        this._age = age;
+    }
+    // method
+    Person.prototype.info = function () {
+        return "".concat(this._name, " - ").concat(this._age, " da");
+    };
+    // static
+    Person.isFlaying = false;
+    return Person;
+}());
+var javlon = new Person("javlon", 22);
+var ismoil = new Person("ismoil", 21);
+// console.log(ismoil);
+// console.log(javlon.info());
+console.log(Person.isFlaying);
--- lesson5/index.ts
+++ lesson5/index.ts
@@ -1,1 +1,28 @@
-console.log('l');
+// TS da sinflar or classes
+// Agar class ni ichida function yozadigon bolsek bu method deb atalaid,
+// Agar class dan tashqarida function yozadigon bolsak bu funtion deb ataladi.
+class Person {
+    _name: string = 'Javlon';
+    _age: number = 21;
+
+    constructor(name: string, age: number) {
+        this._name = name;
+        this._age = age;
+    }
+
+    // method
+    info(): string {
+        return `${this._name} - ${this._age} da`;
+    }
+
+    // static
+    static isFlaying = false;
+}
+
+const javlon: Person = new Person("javlon", 22);
+const ismoil: Person = new Person("ismoil", 21);
+// console.log(ismoil);
+// console.log(javlon.info());
+console.log(Person.isFlaying);
+
+
