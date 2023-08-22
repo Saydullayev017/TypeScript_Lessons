c33a640a5679585462f42f66ae45226f53c9fa1a
Javlonbek Saydullayev <88250263+Saydullayev017@users.noreply.github.com>
2023-08-22 17:03:26 +0300

    lesson5

--- lesson5/index.js
+++ lesson5/index.js
@@ -12,6 +12,35 @@ var Person = /** @class */ (function () {
     Person.prototype.info = function () {
         return "".concat(this._name, " - ").concat(this._age, " da");
     };
+    Person.description = function () {
+        return 'Bu klass person haqida ...';
+    };
+    Object.defineProperty(Person.prototype, "name", {
+        // getter and setter
+        get: function () {
+            return this._name;
+        },
+        set: function (value) {
+            this._name = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Person.prototype, "age", {
+        get: function () {
+            return this._age;
+        },
+        set: function (value) {
+            if (value > 0 && value < 100) {
+                this._age = value;
+            }
+            else {
+                throw new Error("SIZ NO TOG'RI YOSH KRITDINGIZ");
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
     // static
     Person.isFlaying = false;
     return Person;
@@ -20,4 +49,16 @@ var javlon = new Person("javlon", 22);
 var ismoil = new Person("ismoil", 21);
 // console.log(ismoil);
 // console.log(javlon.info());
-console.log(Person.isFlaying);
+// console.log(Person.isFlaying);
+// console.log(Person.description());
+// console.log(javlon);
+// javlon.age = 25;
+// console.log(javlon.age);
+// instanceof
+// bu class or objectni tekshirish uchun ishlatiladi
+if (javlon instanceof Person) {
+    console.log('Tegishli shu objectga');
+}
+else {
+    console.log('Tegishli emas');
+}
--- lesson5/index.ts
+++ lesson5/index.ts
@@ -17,12 +17,46 @@ class Person {
 
     // static
     static isFlaying = false;
+    static description(): string {
+        return 'Bu klass person haqida ...'
+    }
+
+    // getter and setter
+    get name(): string {
+        return this._name;
+    }
+    set name(value: string) {
+        this._name = value;
+    } 
+
+    get age(): number {
+        return this._age;
+    }
+    set age(value: number) {
+        if(value > 0 && value < 100) {
+            this._age = value;
+        }else {
+            throw new Error("SIZ NO TOG'RI YOSH KRITDINGIZ")
+        }
+    }
 }
 
 const javlon: Person = new Person("javlon", 22);
 const ismoil: Person = new Person("ismoil", 21);
 // console.log(ismoil);
 // console.log(javlon.info());
-console.log(Person.isFlaying);
+// console.log(Person.isFlaying);
+// console.log(Person.description());
 
+// console.log(javlon);
+// javlon.age = 25;
+// console.log(javlon.age);
 
+// instanceof
+// bu class or objectni tekshirish uchun ishlatiladi
+if(javlon instanceof Person) {
+    console.log('Tegishli shu objectga');
+}else {
+    console.log('Tegishli emas');
+    
+}
